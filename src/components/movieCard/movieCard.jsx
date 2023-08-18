1642fb4bf09d03121fd0a8a3d0498167bd20e9eb
RenanFernandoRuiz <renanfernandor908@gmail.com>
2023-08-18 17:03:31 -0300

    finalizamos o movie

--- src/components/movieCard/movieCard.jsx
+++ src/components/movieCard/movieCard.jsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
+import MovieDesc from "../movieDescription/movieDescription";
 import styles from "./movieCard.module.css";
 
-const MovieCard = ({movies}) => {
+const MovieCard = ({ movies }) => {
     const movie = movies;
-    return(
-      <div className={styles.movie}>
-        <div>
-            <p>{movie.Year}</p>
-        </div>
-        <div>
-            <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
-        </div>
-        <div>
-            <span>{movie.Type}</span>
-            <h3>{movie.Title}</h3>
-        </div>
-      </div>
-    )
-}
+    const [isModalOpen,  setIsModalOpen] = useState(false);
+
+    const toggleModal= () => {
+        setIsModalOpen(!isModalOpen);
+    }
+    return (
+        <>
+            <div className={styles.movie} onClick={toggleModal}>
+                <div>
+                    <p>{movie.Year}</p>
+                </div>
+                <div>
+                    <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} alt={movie.Title} />
+                </div>
+                <div>
+                    <span>{movie.Type}</span>
+                    <h3>{movie.Title}</h3>
+                </div>
+            </div>
+           {isModalOpen &&  <MovieDesc movies={movie}  click={toggleModal} />}
+        </>
+    );
+};
 
 export default MovieCard;
---
+++ src/components/movieDescription/movieDescription.jsx
@@ -0,0 +1,26 @@
+import styles from "./movieDescription.module.css";
+import devFlix from "/favicon.png";
+
+const MovieDesc = ({ movies, click }) => {
+
+    const movie = movies;
+    return (
+        <div className={styles.modalBackdrop} onClick={click}> 
+            <div className={styles.movieModal} onClick={(e) => e.stopPropagation()}>
+                <div className={styles.movieInfo}>
+                    <img src={movie.Poster} alt={movie.Title} />
+                    <button className={styles.btnClose} onClick={click}>X</button>
+                     <div className={styles.movieType}>
+                        <div>
+                     <img src={devFlix} alt="Logo DEVFLIX" />
+                     {movie.Type}
+                     <h1>{movie.Title}</h1>
+                     </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MovieDesc;
